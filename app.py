c5cff709f08eb5bd5140f317c7802a188703d868
gunpreet016 <gunpreetkaur16052007@gmail.com>
2026-07-31 13:16:50 +0530

    app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ elif all(ALL_API):
     model = st.sidebar.selectbox("Gemini-Model-Name",
                                  options = ["gemini-2.5-flash",
                                             "gemini-2.5-flash-lite",
-                                            "gemini-3.5-flash,
+                                            "gemini-3.5-flash",
                                             "gemini-3.5-flash-lite"])
   )
 else:
